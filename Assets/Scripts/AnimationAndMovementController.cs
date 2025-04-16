8e206a6da558879d4b68ac9753701c4b971d88ae
Mei <90848768+meialbert29@users.noreply.github.com>
2025-04-16 13:46:25 +0200

    16th April
    Finishing animations:
    - Running backward
    - Left turn
    - Right turn
    
    Changing camera

--- Assets/Scripts/AnimationAndMovementController.cs
+++ Assets/Scripts/AnimationAndMovementController.cs
@@ -11,6 +11,8 @@ public class AnimationAndMovementController : MonoBehaviour
     // variables to store optimized setter/getter parameters IDs
     int isWalkingHash;
     int isRunningHash;
+    int isJumpingHash;
+    int isWalkingBackwardHash;
 
     // variables to store player input values
     Vector2 currentMovementInput;
@@ -18,11 +20,16 @@ public class AnimationAndMovementController : MonoBehaviour
     Vector3 currentRunMovement;
     bool isMovementPressed;
     bool isRunPressed;
+    bool isTurningLeft;
+    bool isTurningRight;
+    bool isMovingForward;
+    bool isMovingBackward;
 
     // constants
     float rotationFactorPerFrame = 15.0f;
     float runMultiplier = 3.0f;
     int zero = 0;
+    public float turnSpeed = 100f; // grados por segundo
 
     //gravity variables
     float gravity = -9.8f;
@@ -34,6 +41,7 @@ public class AnimationAndMovementController : MonoBehaviour
     float maxJumpHeight = 4.0f;
     float maxJumpTime = 0.75f;
     bool isJumping = false;
+    bool isJumpAnimating = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Awake()
@@ -44,6 +52,8 @@ public class AnimationAndMovementController : MonoBehaviour
 
         isWalkingHash = Animator.StringToHash("isWalking");
         isRunningHash = Animator.StringToHash("isRunning");
+        isJumpingHash = Animator.StringToHash("isJumping");
+        isWalkingBackwardHash = Animator.StringToHash("isWalkingBackward");
 
         // reads when the buttons are pressed or not
         playerInput.CharacterControls.Move.started += onMovementInput;
@@ -70,6 +80,8 @@ public class AnimationAndMovementController : MonoBehaviour
     {
         if (!isJumping && characterController.isGrounded && isJumpPressed)
         {
+            animator.SetBool(isJumpingHash, true);
+            isJumpAnimating = true;
             isJumping = true;
             currentMovement.y = initialJumpVelocity * 0.5f;
             currentRunMovement.y = initialJumpVelocity * 0.5f;
@@ -93,21 +105,32 @@ public class AnimationAndMovementController : MonoBehaviour
 
     void handleRotation()
     {
-        Vector3 positionToLookAt;
-        // the change in position the character should point to
-        positionToLookAt.x = currentMovement.x;
-        positionToLookAt.y = 0.0f;
-        positionToLookAt.z = currentMovement.z;
+        //Vector3 positionToLookAt;
+        //// the change in position the character should point to
+        //positionToLookAt.x = currentMovement.x;
+        //positionToLookAt.y = 0.0f;
+        //positionToLookAt.z = 0.0f;
 
-        // the current rotation of the character
-        Quaternion currentRotation = transform.rotation;
+        //// the current rotation of the character
+        //Quaternion currentRotation = transform.rotation;
 
-        if (isMovementPressed)
+        //if (isMovementPressed)
+        //{
+        //    // creates a new rotation based on where the player is currently pressing
+        //    Quaternion targetRotation = Quaternion.LookRotation(positionToLookAt);
+        //    // rotate the character to face the positionToLookAt
+        //    transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, rotationFactorPerFrame * Time.deltaTime);
+        //}
+
+        float rotationThisFrame = turnSpeed * Time.deltaTime;
+
+        if (isTurningLeft)
         {
-            // creates a new rotation based on where the player is currently pressing
-            Quaternion targetRotation = Quaternion.LookRotation(positionToLookAt);
-            // rotate the character to face the positionToLookAt
-            transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, rotationFactorPerFrame * Time.deltaTime);
+            transform.Rotate(0, -rotationThisFrame, 0);
+        }
+        else if (isTurningRight)
+        {
+            transform.Rotate(0, rotationThisFrame, 0);
         }
     }
     void onMovementInput(InputAction.CallbackContext context)
@@ -115,26 +138,31 @@ public class AnimationAndMovementController : MonoBehaviour
         currentMovementInput = context.ReadValue<Vector2>();
         currentMovement.x = currentMovementInput.x;
         currentMovement.z = currentMovementInput.y;
-        currentRunMovement.x = currentMovementInput.x * runMultiplier;
-        currentRunMovement.z = currentMovementInput.y * runMultiplier;
-        isMovementPressed = currentMovementInput.x != zero || currentMovementInput.y != zero;
+        currentMovement.x = currentMovementInput.x * runMultiplier;
+        currentMovement.z = currentMovementInput.y * runMultiplier;
+        
+        isTurningLeft = currentMovementInput.x < 0;
+        isTurningRight = currentMovementInput.x > 0;
+        isMovingForward = currentMovementInput.y > 0;
+        isMovingBackward = currentMovementInput.y < 0;
+
+        isMovementPressed = currentMovementInput.x != zero || currentMovementInput.y != zero || isTurningLeft || isTurningRight;
     }
 
     void handleAnimation()
     {
         bool isWalking = animator.GetBool(isWalkingHash);
         bool isRunning = animator.GetBool(isRunningHash);
+        bool isWalkingBackward = animator.GetBool(isWalkingBackwardHash);
 
-        if (isMovementPressed && !isWalking)
+        if ((isMovementPressed && isMovingForward) && !isWalking)
         {
             animator.SetBool(isWalkingHash, true);
         }
-
         else if(!isMovementPressed && isWalking)
         {
             animator.SetBool(isWalkingHash, false);
         }
-
         if((isMovementPressed && isRunPressed) && !isRunning)
         {
             animator.SetBool(isRunningHash, true);
@@ -142,6 +170,14 @@ public class AnimationAndMovementController : MonoBehaviour
         else if((!isMovementPressed || !isRunPressed) && isRunning)
         {
             animator.SetBool(isRunningHash, false);
+        }
+        if((isMovementPressed && isMovingBackward) && !isWalkingBackward)
+        {
+            animator.SetBool(isWalkingBackwardHash, true);
+        }
+        else if(!isMovementPressed && isWalkingBackward)
+        {
+            animator.SetBool(isWalkingBackwardHash, false);
         }
     }
 
@@ -153,6 +189,12 @@ public class AnimationAndMovementController : MonoBehaviour
         // apply proper gravity depending on if the character is grounded or not
         if (characterController.isGrounded)
         {
+            if (isJumpAnimating)
+            {
+                animator.SetBool(isJumpingHash, false);
+                isJumpAnimating = false;
+            }
+            
             currentMovement.y = groundedGravity;
             currentRunMovement.y = groundedGravity;
         }
@@ -179,6 +221,17 @@ public class AnimationAndMovementController : MonoBehaviour
     {
         handleRotation();
         handleAnimation();
+
+        if (isMovingForward)
+        {
+            Vector3 moveDirection = transform.forward;
+            characterController.Move(moveDirection * Time.deltaTime * (isRunPressed ? runMultiplier : 1f));
+        }
+        else if(isMovingBackward)
+        {
+            Vector3 moveDirection = transform.forward;
+            characterController.Move( -moveDirection * Time.deltaTime);
+        }
 
         if (isRunPressed)
         {
---
+++ Assets/Scripts/Camera Scripts/ControllerStatics.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class ControllerStatics
+{
+    public static string horizontal = "Horizontal";
+    public static string vertical = "Vertical";
+    public static string MouseX = "Mouse X";
+    public static string MouseY = "Mouse Y";
+    public static string Sprint = "Sprint";
+}
