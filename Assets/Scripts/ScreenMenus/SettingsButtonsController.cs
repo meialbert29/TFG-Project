cea9ef61567f0771fe2c8d0a7ab6252dced37c81
Mei <90848768+meialbert29@users.noreply.github.com>
2025-06-02 10:51:24 +0200

    Settings menu

---
+++ Assets/Scripts/ScreenMenus/SettingsButtonsController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class SettingsButtonsController : MonoBehaviour
+{
+    // images variables
+    public Image saveButton;
+    public TMP_Text saveText;
+    // sprites variables
+    public Sprite normal_Sprite;
+    public Sprite hover_Sprite;
+
+    private Color normalColor = ColorsPalette.ButtonsColors.normalColor;
+    private Color hoverColor = ColorsPalette.ButtonsColors.hoverColor;
+
+    public void OnSaveButtonEnter()
+    {
+        if (saveButton != null)
+            saveButton.sprite = hover_Sprite;
+        if (saveText != null)
+            saveText.color = hoverColor;
+    }
+    public void OnSaveButtonExit()
+    {
+        if (saveButton != null)
+            saveButton.sprite = normal_Sprite;
+        if (saveText != null)
+            saveText.color = normalColor;
+    }
+}
