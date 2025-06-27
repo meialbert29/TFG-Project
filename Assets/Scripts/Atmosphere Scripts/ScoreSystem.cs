be920ae0516852dc4483b820c2e4ba343986884b
Mei <90848768+meialbert29@users.noreply.github.com>
2025-06-27 14:13:01 +0200

    finished

--- Assets/Scripts/Atmosphere Scripts/ScoreSystem.cs
+++ Assets/Scripts/Atmosphere Scripts/ScoreSystem.cs
@@ -42,7 +42,7 @@ public class ScoreSystem : MonoBehaviour
 
     void Update()
     {
-        HandleInputPoints();
+        //HandleInputPoints();
         UpdateMoodPoints();
         UpdatePointsEarned();
         UpdateScorePoints();
